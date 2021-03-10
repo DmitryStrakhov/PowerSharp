1b4f5fc14776cfc70152cdb66e279a861698f6f6
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2021-03-11 01:09:00 +0300

    #Infrastructure

---
+++ Src/PowerSharp/ContextActions/FieldInstance/CreateFieldInstanceContextAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JetBrains.ProjectModel;
+using JetBrains.TextControl;
+using JetBrains.Util;
+using JetBrains.Application.Progress;
+using JetBrains.ReSharper.Feature.Services.ContextActions;
+
+namespace PowerSharp.ContextActions.FieldInstance {
+    [ContextAction(Name = "CreateFieldInstance", Description = "Creates instance of a field", Group = "C#", Disabled = false, Priority = 1)]
+    public class CreateFieldInstanceContextAction : ContextActionBase {
+        public CreateFieldInstanceContextAction(LanguageIndependentContextActionDataProvider dataProvider) {
+        }
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress) {
+            return x => MessageBox.ShowInfo(nameof(CreateFieldInstanceContextAction));
+        }
+        public override bool IsAvailable(IUserDataHolder cache) {
+            return true;
+        }
+        public override string Text { get { return "Create Instance"; } }
+    }
+}
