34a7830acbd26214fa4cf5073be1064a9ef31928
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2024-12-13 19:25:21 +0300

    Rider plugin: migration 2024.2->2024.3

---
+++ Metadata.Rider/PowerSharp/EditParametersAction.java
@@ -0,0 +1,9 @@
+package PowerSharp;
+
+import com.jetbrains.rider.actions.base.RiderAnAction;
+
+public class EditParametersAction extends RiderAnAction {
+    public EditParametersAction() {
+        super("EditParameters", "Edit Parameters", null, null, null);
+    }
+}
---
+++ Metadata.Rider/PowerSharp/EditReturnValueAction.java
@@ -0,0 +1,9 @@
+package PowerSharp;
+
+import com.jetbrains.rider.actions.base.RiderAnAction;
+
+public class EditReturnValueAction extends RiderAnAction {
+    public EditReturnValueAction() {
+        super("EditReturnValue", "Edit Return Value", null, null, null);
+    }
+}
