c54612aa715a3b5b7241a1229b21bedbe537ff02
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2025-08-31 01:42:58 +0300

    #Rider plugin: explicit closing popups after 'ForceMethodCall' command execution

--- Metadata.Rider/PowerSharp/ForceMethodCallAction.java
+++ Metadata.Rider/PowerSharp/ForceMethodCallAction.java
@@ -1,9 +1,17 @@
 package PowerSharp;
 
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.jetbrains.rider.actions.base.RiderAnAction;
+import com.intellij.ide.IdeEventQueue;
+import org.jetbrains.annotations.NotNull;
 
 public class ForceMethodCallAction extends RiderAnAction {
     public ForceMethodCallAction() {
         super("ForceMethodCall", "Force Method Call", null, null, null);
+    }
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        super.actionPerformed(e);
+        IdeEventQueue.getInstance().getPopupManager().closeAllPopups();
     }
 }
